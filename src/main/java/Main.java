4bb63cd997f5837370336de206a00e7b9d01a6b6
dev <alexandersh@wizardsdev.com>
2022-05-15 01:27:05 +0300

    debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Objects;
 
 public class Main {
 
@@ -12,8 +11,36 @@ public class Main {
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
-//            while (true) {
+            while (true) {
                 clientSocket = serverSocket.accept();
+
+                ClientHandler clientSock
+                        = new ClientHandler(clientSocket);
+
+                new Thread(clientSock).start();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        // Constructor
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
             BufferedWriter out = null;
             BufferedReader in = null;
             try {
@@ -43,63 +70,6 @@ public class Main {
                     e.printStackTrace();
                 }
             }
-
-
-//                ClientHandler clientSock
-//                        = new ClientHandler(clientSocket);
-//
-//                new Thread(clientSock).start();
-//            }
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
         }
     }
-
-//    private static class ClientHandler implements Runnable {
-//        private final Socket clientSocket;
-//
-//        // Constructor
-//        public ClientHandler(Socket socket) {
-//            this.clientSocket = socket;
-//        }
-//
-//        public void run() {
-//            PrintWriter out = null;
-//            BufferedReader in = null;
-//            try {
-//                out = new PrintWriter(new ObjectOutputStream(clientSocket.getOutputStream()));
-//                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-////                while (Objects.nonNull(s = in.readLine())) {
-//                    while (clientSocket.isConnected()) {
-//                        String s = in.readLine();
-//                        System.out.println(s);
-//                        if (s.contains("ping")) {
-//                            out.write("+PONG" + "\r\n");
-//                        }
-//                    }
-//                    out.flush();
-//                    out.close();
-//                    in.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } finally {
-//                try {
-//
-//                        clientSocket.close();
-//
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//        }
-//    }
 }
