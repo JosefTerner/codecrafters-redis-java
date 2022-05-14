224f1614b735d4601e7cdb071c490ad6b49cc617
dev <alexandersh@wizardsdev.com>
2022-05-15 01:25:24 +0300

    debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,22 +16,22 @@ public class Main {
                 clientSocket = serverSocket.accept();
             BufferedWriter out = null;
             BufferedReader in = null;
-            try {                while (clientSocket.isConnected()) {
-
+            try {
                 out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 //                while (Objects.nonNull(s = in.readLine())) {
-//                while (clientSocket.isConnected()) {
-                String s = in.readLine();
-                System.out.println(s);
-                if (s.contains("ping")) {
-                    out.write("+PONG" + "\r\n");
+                while (clientSocket.isConnected()) {
+                    String s = in.readLine();
+                    System.out.println(s);
+                    if (s.contains("ping")) {
+                        out.write("+PONG" + "\r\n");
+                        out.flush();
+                        out.close();
+                    }
                 }
-//                }
                 out.flush();
                 out.close();
                 in.close();
-            }
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
